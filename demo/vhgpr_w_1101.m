938a1a0435a1a21433c6f2d35f26a9fb05e671a0
wwvt <40003901+wwvt@users.noreply.github.com>
2018-09-19 15:34:57 -0400

    Update vhgpr_w_1101.m

--- demo/vhgpr_w_1101.m
+++ demo/vhgpr_w_1101.m
@@ -10,7 +10,7 @@ function [out1, out2, mutst, diagSigmatst, atst, diagCtst]= vhgpr_w_1101(LambdaT
 %                        2 - Hyperparameters for f and g are fixed. (Only
 %                        variational parameters are learned).
 %           - X: Training input data. One vector per row.
-%           - y: Training output value. One scalar per row.
+%           - y: Training output value. One structure per row.
 %           - Xtst: Test input data. One vector per row.
 %
 % Output:   - out1: 
@@ -27,7 +27,7 @@ function [out1, out2, mutst, diagSigmatst, atst, diagCtst]= vhgpr_w_1101(LambdaT
 %           - diagCtst: Variance of the approximate posterior for f.
 %
 % Modes:    Testing (all inputs are given) / Training (last input is omitted)
-%
+
 
 [n,D] = size(X); 
 
@@ -79,10 +79,6 @@ mu = Kg*(  DD*ones(n,1)-0.5*nV)+ ones(n,1).*delta0; %ones(n,1) * 0;
     % MlogDet = log(det(M)); % same
   yy=[];
     
-%     BB = A * Rinv * A';
-%         BBinv = inv(BB);
-
-        %BB = Rdiaginv*exp(-theta2(end));   %*************
         
         BB = inv(Phi);
         BBinv = Phi;
@@ -90,10 +86,6 @@ mu = Kg*(  DD*ones(n,1)-0.5*nV)+ ones(n,1).*delta0; %ones(n,1) * 0;
 p = 1e-3; % This value doesn't affect the result, it is a scaling factor
 scale = 1./sqrt(p+diag(R)); 
 Rscale = 1./(1+p./diag(R));
-% Ls = chol((A'*Kf*A).*(scale*scale')+diag(Rscale));     % O(n^3)
-% Lys = (Ls'\(yy'.*scale));
-% alphascale = Ls\Lys;
-% alpha = alphascale.*scale;       
         
     IBKinv = (eye(n) + BB * Kf)\eye(n); %singular
    
@@ -105,19 +97,16 @@ Rscale = 1./(1+p./diag(R));
   Minv = Rinv - Rinv * A' * Kf * Ginv * Phi * A * Rinv;
   F1 = -0.5*(nn*log(2*pi)+MlogDet+(yy-mu0) * Minv * (yy-mu0)'); % O(n3) same.
   F2 = -0.25 .* nV' * diag(Omega);
-  F3 = -KLdiv(mu,repelem(delta0,n)',Omega,Kg);  %*************************** KLdiv definition corrected
+  F3 = -KLdiv(mu,repelem(delta0,n)',Omega,Kg); 
 
   F = F1+F2+F3;
   out1 = -F;
 
-% --- Derivatives   % Note: VBGP Rcode uses optim() method ' SANN, which does not consider the derivatives. 
-if nargin == 7 && nargout == 2  % training, getting [LambdaTheta, convergence0] = minimize() %6->7 because of A as input 
-                                % in minimize.m function, [f0 df0] = feval(f, X, varargin{:});          % get function value and gradient
-                                % need to find the derivatives. The old one
-                                % does not work
+% --- Derivatives 
+if nargin == 7 && nargout == 2  
+
 out2=zeros(n+1+ntheta1+ntheta2,1);
-%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    
+   
     % wrt Lambda
 
 
@@ -137,38 +126,31 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
             dRD(j,j) = dRDi ;
             end
                    
-             dBDtemp=(-diag(nV) * Rdiaginv * dRD * Rdiaginv);
-             dBD  = dBDtemp;  %**********************
-%           dBD  = -diag(1./nV) * Rdiaginv *  dRD * Rdiaginv;  %**********************
-
+             dBD=(-diag(nV) * Rdiaginv * dRD * Rdiaginv);
              dFLambda(i)=  trace(-dBD*BBinv)+ trace(IBKinv*dBD* Kf) +  ... 
                 bd * dBD * (1./nV) +(-1)*(Y' * (eye(n) - BB * Kf * IBKinv) * dBD * Kf * IBKinv * BB' * Y ) +...
                 (-1)* Y'* BB * Kf*  IBKinv * dBD* Y   + ...
                            trace(Omega * dDD) -  trace(Omega * dDD * Omega * inv(Kg) ) + ...
                            trace((Kg * DD * ones(n,1)*ones(n,1)'+ ones(n,1)*ones(n,1)'*DD * Kg -ones(n,1)*ones(n,1)'*Kg)*dDD)- 1/2 *trace(Omega * dDD *  Omega);
       end
-    out2(1:n) = Lambda.*dFLambda'/2;   %**************************
-                    %  
-
- 
+    out2(1:n) = Lambda.*dFLambda'/2;   
  %<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    if fixhyp < 1       % fixhyp = 0, do this. 
+    if fixhyp < 1      
     % wrt Kf hyperparameters
             for k = 1:ntheta1
              dKthetaf  = feval(covfunc1{:}, theta1, X, k);
-             dFthetaf = trace(IBKinv * BB*dKthetaf) - (Y' * inv((BBinv+Kf)) * dKthetaf * inv((BBinv+Kf)) * Y ); %same
+             dFthetaf = trace(IBKinv * BB*dKthetaf) - (Y' * inv((BBinv+Kf)) * dKthetaf * inv((BBinv+Kf)) * Y ); 
              out2(n+k) = dFthetaf/2;
             end
     end
     
  
-    if fixhyp < 2         %  fixhyp = 0, do this
+    if fixhyp < 2   
     % wrt Kg hyperparameters
     for k = 1:ntheta2    
     dKthetag = feval(covfunc2{:}, theta2, X, k); 
-    temp = dKthetag * (DD * ones(n,1)-0.5*nV);  %*********************
-    temp2  = IKDinv * dKthetag * IKDinv; %theoretically same, numerically different
-       
+    temp = dKthetag * (DD * ones(n,1)-0.5*nV);  
+    temp2  = IKDinv * dKthetag * IKDinv;
    
        dRthetag = zeros(n);
             for i = 1:n
@@ -179,7 +161,7 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
 
             dRthetag(i,i) = dRthetagi;
             end
-    dBdthetag = -diag(nV) * Rdiaginv * dRthetag * Rdiaginv; %*************************
+    dBdthetag = -diag(nV) * Rdiaginv * dRthetag * Rdiaginv; 
     dFthetag= trace(-dBdthetag*BBinv)+ trace(IBKinv*dBdthetag* Kf) + bd * dBdthetag * (1./nV) + ...
                (-1)*(Y' * (eye(n) - BB * Kf * IBKinv) * dBdthetag * Kf * IBKinv * BB' * Y ) +...
                (-1)* Y'* BB * Kf*  IBKinv * dBdthetag* Y + ...
@@ -189,10 +171,7 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
                -1/4* nV' * dKthetag *  nV + ones(n,1)' * dKthetag *(nV/2);
         out2(n+ntheta1+k) = dFthetag/2;
     end
-    
-    
-%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+   
     % wrt mu0
 
 
@@ -203,10 +182,8 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
     dRmu0(i,i) = dRmu0i;
  end
     
-  %  dBmu0 = -diag(1./nV)  *Rdiaginv  *  dRmu0 * Rdiaginv; %**********************************
-    dBmu0 = -diag(nV)  *Rdiaginv  *  dRmu0 * Rdiaginv; %**********************************
-    
-   dFmu0= trace(-BBinv* dBmu0)+ trace(IBKinv*dBmu0* Kf) + bd * dBmu0 * (1./nV) + ...
+    dBmu0 = -diag(nV)  *Rdiaginv  *  dRmu0 * Rdiaginv;     
+    dFmu0= trace(-BBinv* dBmu0)+ trace(IBKinv*dBmu0* Kf) + bd * dBmu0 * (1./nV) + ...
                (-1)*(Y' * (eye(n) - BB * Kf * IBKinv) * dBmu0 * Kf * IBKinv * BB' * Y ) +...
                (-1)* Y'* BB * Kf*  IBKinv * dBmu0* Y + (nV - ones(n,1))'*ones(n,1);
     out2(n+ntheta1+ntheta2+1) = dFmu0/2;
@@ -214,14 +191,13 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
     
     
 % --- Predictions
-  %-----------------WW edited ---------------%
- elseif nargin == 8  % add A as input
+ elseif nargin == 8  
     [K1ss, K1star] = feval(covfunc1{:}, theta1, X, Xtst);     % test covariance f
     [K2ss, K2star] = feval(covfunc2{:}, theta2, X, Xtst);     % test covariance g
   rf = K1star;
   rdelta = K2star;
   pa = mu0 + (rf)' * Ginv * (Y-repelem(mu0,n)');
-  pb = delta0 + (rdelta)'*(diag(Lambda)-0.5.*diag(nV))*repelem(1,n)';  % with replication VBGPpredict 3
+  pb = delta0 + (rdelta)'*(diag(Lambda)-0.5.*diag(nV))*repelem(1,n)';  
   atst = pa; 
   mutst= pb;  
   out1 = atst;                                              % predicted mean  y
@@ -231,7 +207,5 @@ out2=zeros(n+1+ntheta1+ntheta2,1);
   diagCtst=pc;
   diagSigmatst = pd;
   out2 = diagCtst + exp(mutst+diagSigmatst/2);              % predicted variance y   = pc + exp(pb+pd/2);; % predicted variance y
-  % out2 = exp(mutst+diagSigmatst/2);              %  mean of variance log normal   = exp(pb+pd/2)
 end
-  %-----------------WW edited ---------------%
  end
